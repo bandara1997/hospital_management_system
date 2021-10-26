0820766317fd296f4e69515cf11b18bcaaaf3a70
Keshara Bandara <88639083+bandara1997@users.noreply.github.com>
2021-10-26 08:29:27 +0530

    Add files via upload

---
+++ Hospital Management System/src/Project/ConnectionProVider.java
@@ -0,0 +1,28 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+
+/**
+ *
+ * @author Kavinda
+ */
+public class ConnectionProVider {
+    public static Connection getCon()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/hmss","root","");
+            return con;
+        }
+        catch(Exception e)
+        {
+            return null;        
+        }
+    }
+    
+}
